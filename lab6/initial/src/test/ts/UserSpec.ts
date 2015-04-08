2017e856fd4b6f40b6965f72c58b4fa87632b946
Kai Toedter <kai@toedter.com>
2015-04-08 19:47:50 +0200

    lab 6: added initial

---
+++ lab6/initial/src/test/ts/UserSpec.ts
@@ -0,0 +1,13 @@
+/// <reference path="../../../typings/jasmine/jasmine.d.ts" />
+/// <reference path="../../main/ts/User.ts" />
+/// <reference path="../../main/ts/SimpleUser.ts" />
+
+describe('User', () => {
+    it('should create user and get attributes', () => {
+        var user:tutorial.webapp.User = new tutorial.webapp.SimpleUser("user1", "user1@test.com", "User 1");
+        expect(user).toBeDefined();
+        expect(user.getId()).toBe('user1');
+        expect(user.getEmail()).toBe('user1@test.com');
+        expect(user.getFullName()).toBe('User 1');
+    });
+});
