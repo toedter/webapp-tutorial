e9ca53bb3c537f7aa68b4aeebdde163fb4f44cbb
Kai Toedter <kai@toedter.com>
2016-05-29 13:02:47 +0200

    cleanup

--- lab5/complete/src/main/webapp/app/user/users-service.ts
+++ lab5/complete/src/main/webapp/app/user/users-service.ts
@@ -1,7 +1,4 @@
 import {Injectable} from '@angular/core';
-import {Http, Response} from '@angular/http';
-import 'rxjs/add/operator/map';
-import {Observable} from 'rxjs/Observable';
 import {User} from './user';
 
 @Injectable()
