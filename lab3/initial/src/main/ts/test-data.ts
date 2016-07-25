9494c9edcf8678eaf31a4d527e393cf354e2a168
Kai Toedter <kai@toedter.com>
2016-07-25 19:00:28 +0200

    fixed initial lab3 test-data.ts

--- lab3/initial/src/main/ts/test-data.ts
+++ lab3/initial/src/main/ts/test-data.ts
@@ -2,10 +2,4 @@
 ///<reference path='simple-user.ts'/>
 
 namespace tutorial.webapp {
-    export function createTestUsers(): User[] {
-        let users: User[] = [];
-        // let testUser1: User = new SimpleUser('user1', 'user1@test.com', 'User 1');
-        // users.push(testUser1);
-        return users;
-    }
 }
