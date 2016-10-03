61f9f48c6e1ad90faa114f0c4972d5e0804c47ed
Kai Toedter <kai@toedter.com>
2016-10-03 13:44:59 +0200

    - updated to spring boot 1.4.1 - improved spring boot tests

--- lab1/complete/src/test/java/com/toedter/tutorials/webapp/lab1/HelloControllerTest.java
+++ lab1/complete/src/test/java/com/toedter/tutorials/webapp/lab1/HelloControllerTest.java
@@ -8,17 +8,18 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockServletContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = MockServletContext.class)
-@WebAppConfiguration
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = MockServletContext.class)
 public class HelloControllerTest {
 
 	private MockMvc mockMVC;
@@ -29,9 +30,9 @@ public class HelloControllerTest {
 	}
 
 	@Test
-	public void getHello() throws Exception {
+	public void shouldGetHello() throws Exception {
 		mockMVC.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk())
-				.andExpect(content().string(equalTo(HelloController.LAB1_GREETINGS_FROM_SPRING_BOOT)));
+				.andExpect(content().string(equalTo(HelloController.LAB1_GREETINGS_FROM_SPRING_BOOT )));
 	}
 }
