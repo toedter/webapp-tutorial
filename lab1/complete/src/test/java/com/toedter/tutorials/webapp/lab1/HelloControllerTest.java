f3d351b0732eb3fc798a91596b4f84e2df73c8e7
Kai Toedter <kai@toedter.com>
2017-04-25 21:03:27 +0200

    Update to latest nodejs dependencies

--- lab1/complete/src/test/java/com/toedter/tutorials/webapp/lab1/HelloControllerTest.java
+++ lab1/complete/src/test/java/com/toedter/tutorials/webapp/lab1/HelloControllerTest.java
@@ -1,24 +1,17 @@
 package com.toedter.tutorials.webapp.lab1;
 
-import static org.hamcrest.Matchers.equalTo;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockServletContext;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(HelloController.class)
---
+++ tools/ncu-all.bat
@@ -0,0 +1,23 @@
+cd ..
+call ncu -u
+cd lab3\initial
+call ncu -u
+cd ..\complete
+call ncu -u
+cd ..\..\lab4\initial
+call ncu -u
+cd ..\complete
+call ncu -u
+cd ..\..\lab5\initial
+call ncu -u
+cd ..\complete
+call ncu -u
+cd ..\..\lab6\initial
+call ncu -u
+cd ..\complete
+call ncu -u
+cd ..\..\lab7\initial
+call ncu -u
+cd ..\complete
+call ncu -u
+cd ..\..
