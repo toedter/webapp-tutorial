1bd8bd2876c3124e2a51f9ac3aa16b8e4f666b3f
Kai Toedter <kai@toedter.com>
2018-07-21 15:25:14 +0200

    chore: fixed gradle nodejs setup in all labs

--- lab6/complete/backup/src/main/java/com/toedter/tutorials/webapp/lab6/user/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package com.toedter.tutorials.webapp.lab6.user;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface UserRepository extends PagingAndSortingRepository<User, String> {
-}
