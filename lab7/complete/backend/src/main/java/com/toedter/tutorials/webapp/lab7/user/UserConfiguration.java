212cc4a4012628bfe1699256dca22457cc2362dc
Kai Toedter <kai@toedter.com>
2021-04-17 13:27:45 +0200

    chore: update to latest Spring Boot and Angular dependencies

--- lab7/complete/backend/src/main/java/com/toedter/tutorials/webapp/lab7/user/UserConfiguration.java
+++ lab7/complete/backend/src/main/java/com/toedter/tutorials/webapp/lab7/user/UserConfiguration.java
@@ -2,13 +2,15 @@ package com.toedter.tutorials.webapp.lab7.user;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
-import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
 
 @Configuration
-public class UserConfiguration extends RepositoryRestConfigurerAdapter {
+public class UserConfiguration implements RepositoryRestConfigurer {
  
     @Override
-    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry corsRegistry) {
         config.exposeIdsFor(User.class);
     }
 }
