7c8d9729450fb390172eaa06bdc04f31f8424a8d
Kai Toedter <kai@toedter.com>
2023-04-22 13:28:23 +0200

    feat: use JpaRepository for user repository

--- lab7/complete/backend/src/main/java/com/toedter/tutorials/webapp/lab7/user/UserRepository.java
+++ lab7/complete/backend/src/main/java/com/toedter/tutorials/webapp/lab7/user/UserRepository.java
@@ -1,7 +1,6 @@
 package com.toedter.tutorials.webapp.lab7.user;
 
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends CrudRepository<User, String>, PagingAndSortingRepository<User, String> {
+public interface UserRepository extends JpaRepository<User, String> {
 }
